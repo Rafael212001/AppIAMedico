f5d4eebe03400f04813d83eed199f8a46b8e5673
BrunoKKshi <fbruninho2017@gmail.com>
2021-10-29 21:39:40 -0300

    atualização servidor

---
+++ moduloia.py
@@ -0,0 +1,23 @@
+def ia1(sintomas):
+
+    sintoma1 = sintomas['sintomas'][0]
+
+    return jsonify(sintoma1)
+
+def ia2(sintomas):
+
+    sintoma2 = sintomas['sintomas'][1]
+
+    return jsonify(sintoma2)
+
+def ia3(sintomas):
+
+    sintoma3 = sintomas['sintomas'][2]
+
+    return jsonify(sintoma3)
+
+def ia4(sintomas):
+
+    sintoma4 = sintomas['sintomas'][3]
+
+    return jsonify(sintoma4)
